eb0ff599a9409acc4f409c06d2daa5b5c8acc9a1
karagota <karagota@gmail.com>
2016-07-20 05:51:37 +0300

    dummy config

---
+++ poroshki/config.php
@@ -0,0 +1,15 @@
+<?php
+$db_name = 'dbname';
+$db_remotehost = "dbremote";
+$db_host = "localhost";
+$db_user = "dbuser";
+$db_pass = "dbpass";
+$dumpdir = "dump";
+$server_name = "server.name"; 
+$domain = "http://".$server_name;
+$sitename = 'My Shiny Project';
+$included_in_backup = "folder index.php robots.txt favicon.ico .htaccess ";
+$server_time_zone='Europe/Paris';
+$vip_id = "42";
+$admin_email='admin@server.name';
+?>
