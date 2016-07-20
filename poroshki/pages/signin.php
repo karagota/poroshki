e0b028cfa27fa1b99441517449af74b04af63f96
root <root@subscribe.runamail.ru>
2016-07-20 17:59:45 +0300

    new signin.php

--- poroshki/pages/signin.php
+++ poroshki/pages/signin.php
@@ -1,7 +1,6 @@
 <div class="col-xs-9" <?php /*style="border-right:#DDD 1px solid;"*/?> >
 	<div style="width:300px;height:500px;">
-<!--<script src="//ulogin.ru/js/ulogin.js"></script><div id="uLogin1" data-uloginid="a32fc3ee"></div>-->
-<script src="//ulogin.ru/js/ulogin.js"></script><div id="uLogin_0b7d0f90" data-uloginid="0b7d0f90"></div>  </div>
+<script src="//ulogin.ru/js/ulogin.js"></script><div id="uLogin_<?php echo $ulogin_key; ?>" data-uloginid="<?php echo $ulogin_key; ?>"></div>  </div>
 </div>
 
 
